d9d6ecfc09decd10e378250b91fe8d79f83de170
Kevin Wilson <entemper@gmail.com>
2017-02-28 00:06:56 -0800

    Fixed some stuff, started play area lesson

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
 
-	// Use this for initialization
-	void Start () {
-		GameObject.DontDestroyOnLoad(gameObject);
+	void Awake () {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		}
+		else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
